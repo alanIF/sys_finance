31df9f89d3deadaad798c2b888361adb3e0f4705
Alan Tavares <alan_tj@hotmail.com.br>
2022-01-01 15:13:04 -0300

    controllers, models receita and despesa

---
+++ aplication/app/Models/Despesa.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Despesa extends Model
+{
+    use HasFactory;
+    protected $table= "despesa";
+    protected $fillable= ['descricao','valor','data_despesa', 'user_id'];
+
+}
