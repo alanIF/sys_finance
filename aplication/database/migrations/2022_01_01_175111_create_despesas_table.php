a66c1061ef3b3f2627de782be0a42572da6ce454
Alan Tavares <alan_tj@hotmail.com.br>
2022-01-01 15:07:27 -0300

    create project, bd, login, logout, session

---
+++ aplication/database/migrations/2022_01_01_175111_create_despesas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDespesasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('despesa', function (Blueprint $table) {
+            $table->id();
+            $table->text("descricao");
+            $table->double("valor");
+            $table->text("data_despesa");
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('despesa');
+    }
+}
