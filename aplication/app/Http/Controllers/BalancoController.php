d9fb572a7e15cea8f3a6070f64de083c3907f208
Alan Tavares <alan_tj@hotmail.com.br>
2022-01-03 10:12:01 -0300

    BalancoCOntroller

--- aplication/app/Http/Controllers/BalancoController.php
+++ aplication/app/Http/Controllers/BalancoController.php
@@ -15,15 +15,14 @@ class BalancoController extends Controller
 
         $sql = 'Select * from receita r where r.user_id='.$user.'';
         $receitas = \DB::select($sql);
-
         $receitas_t=array();
-     
+
         $total_m=0;
         $meses=array();
         $mes_atual="00/2022";
         
         // pegando os meses
-        for($i=0; count($receitas);$i++){
+        for($i=0; $i<count($receitas);$i++){
 
             $mes = explode('/',$receitas[$i]->data_receita,2);
             $receitas_m= array();
@@ -32,31 +31,59 @@ class BalancoController extends Controller
                 $receitas_m['data']=$mes[1];
 
                 $receitas_m['total_receita']=0;
-                for($j=0; count($receitas);$j++){
+
+                for($j=0; $j<count($receitas);$j++){
                     //
                     $mes_j = explode('/',$receitas[$j]->data_receita,2);
                     if(strcmp($receitas_m['data'], $mes_j[1])==0){
                         $receitas_m['total_receita']= $receitas[$j]->valor + $receitas_m['total_receita'];
                     }
                 }
-                return dd($receitas_m);
 
                 array_push($receitas_t,$receitas_m);
             }
 
         }
-        return dd($receitas_t);   
-        $balanco['meses']= $meses;
 
 
         $sql = 'Select * from despesa d where d.user_id='.$user.'';
         $despesas = \DB::select($sql);
+        $despesas_t=array();
+
+        $total_m=0;
+        $meses=array();
+        $mes_atual="00/2022";
         
+        // pegando os meses
+        for($i=0; $i<count($despesas);$i++){
+
+            $mes = explode('/',$despesas[$i]->data_despesa,2);
+            $despesas_m= array();
+            if(strcmp($mes[1], $mes_atual)!=0){
+                $mes_atual=$mes[1];
+                $despesas_m['data']=$mes[1];
+
+                $despesas_m['total_receita']=0;
+
+                for($j=0; $j<count($despesas);$j++){
+                    //
+                    $mes_j = explode('/',$despesas[$j]->data_despesa,2);
+                    if(strcmp($despesas_m['data'], $mes_j[1])==0){
+                        $despesas_m['total_receita']= $despesas[$j]->valor + $despesas_m['total_receita'];
+                    }
+                }
+
+                array_push($despesas_t,$despesas_m);
+            }
+
+        }
+
+
+
+
         $balanco = array();
-        // array 0 - receitas array 1 - despesas
-        array_push($balanco,$receitas);
-        array_push($balanco,$despesas);
-        return dd($balanco);
-        return view('receitas.index', ['balanco' => $balanco]);
+        $balanco['receitas']= $receitas_t;
+        $balanco['despesas']= $despesas_t;
+        return view('balanco.index', ['balanco' => $balanco]);
     }
 }
--- aplication/resources/views/balanco/index.blade.php
+++ aplication/resources/views/balanco/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card danger">
-                <div class="card-header">Despesas</div>
+                <div class="card-header">Balanço</div>
 
                 <div class="card-body">
                    
@@ -13,40 +13,19 @@
                     <table class="table table-hover ">
                         <thead>
                             <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">descrição </th>
+                            <th></th>
 
-                            <th scope="col">data </th>
-                            <th scope="col">valor</th>
-                            
-
-                            <th colspan='2'>Ações</th>
-                            
-
+                            @for ($i = 0; $i < count($balanco['receitas']); $i++)
+                                <th>{{$balanco['receitas'][$i]['data']}}</th>
+                            @endfor   
 
                             </tr>
                         </thead>
                         <tbody>
-                        @foreach($despesas as $d)
+                                                   
                         <tr>
 
-                            <td >{{$d->id}}</td>
-                            <td>{{$d->descricao}}</td>
-                            <td>{{$d->data_despesa}}</td>
-
-                            <td>{{$d->valor}}</td>
-
-
-                            <td><a class="btn btn-warning " href="despesas/{{$d->id}}/edit"><i class="fa fa-edit" ></i></a> </td>
-                            <td>   <form action="despesas/delete/{{$d->id}}" method="post"> @csrf @method('delete')<button class="btn btn-danger"><i class="fa fa-trash" ></i></button></form></td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                        <tfoot>
-                            <tr >
-                                <td colspan='6'><a class="btn btn-primary " href="{{url('despesas/new')}}"><i class="fa fa-plus" ></i></a></td>
-
-                            </tr>
+                            
                         </tfoot>
                         </table>
                 </div>
